5b9ce42eded9e9de3aefe387abfd80dd70ff370e
Tanya <tanya@gmail.com>
2023-04-13 15:38:07 +0200

    one change made

--- src/main/java/com/airlineProject/airline/controller/AirLineController.java
+++ src/main/java/com/airlineProject/airline/controller/AirLineController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.airlineProject.airline.model.AirlineTicketing;
@@ -16,7 +17,7 @@ import com.airlineProject.airline.service.AirLineService;
 
 
 @RestController
-
+@RequestMapping("/v1/app")
 public class AirLineController {
 	
 	@Autowired
